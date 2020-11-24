23d3b5bf1094072d69c088074fe4534effae2e6e
Lior Tamir <lior.tamir@gmail.com>
2020-11-24 15:19:31 +0200

    include past lessons in current day as fulure lessons

--- src/components/lesson/UpcomingLessons.js
+++ src/components/lesson/UpcomingLessons.js
@@ -74,11 +74,12 @@ const UpcomingLessons = ({clickHandler, editLesson, changeStatus, doubleClickHan
 	}
 	
 	const filterByDate = (lesson) => {
-		const date = lesson.startDatetime.split(' ')[0];
+		const date = lesson.startDatetime.split(' ')[0] + "T23:59:00";
+		console.log(new Date(date), now);
 		if(showPastUndone === true){
 			return new Date(date) < now
 		} else {
-			return new Date(date) > now
+			return new Date(date) >= now
 		}
 	}
 	return(
