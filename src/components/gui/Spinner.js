1f132d550207c23318d6b7ab95e982908f4d8181
Lior Tamir <lior.tamir@gmail.com>
2020-11-30 13:39:28 +0200

    add small size class and prop

--- src/components/gui/Spinner.js
+++ src/components/gui/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Spinner.css'
 
 const spinner = props =>(
-	<div className="Loader">Loading...</div>
+	<div className={props.sm ? 'LoaderSm' : "Loader"} style={props.margin? {maring: props.margin} : null}>Loading...</div>
 );
 
 export default spinner;
