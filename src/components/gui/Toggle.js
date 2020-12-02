a1631c0099382d37b1f08535070bd14bcfb1b8a6
Lior Tamir <lior.tamir@gmail.com>
2020-12-02 15:52:21 +0200

    max width is the longest text length

--- src/components/gui/Toggle.js
+++ src/components/gui/Toggle.js
@@ -3,19 +3,24 @@ import './Toggle.css'
 
 //'#ce9812'
 const Toggle = ({name, icon, value, sup, title, clickHandler}) => {
+
+	const calcMaxWidth = names => {
+		return name[0].length > name[1].length ? name[0].length : name[1].length;
+	}
+
 	return(
 		<div 
 			className={"Toggle " + (value? "ToggleOff" :  "ToggleOn")} 
-			style={sup? {backgroundColor: 'inherit'} : null} 
+			style={sup? {backgroundColor: 'inherit'} : null, {width: calcMaxWidth(name)/1.6 + 'em'}} 
 			title={title} 
 			onClick={clickHandler?clickHandler:null}>
 
 			{icon && <span className="material-icons">{icon}</span>}
-			<span 
-				className="FieldData" 
+			<div 
+				className="ToggleData" 
 				style={sup?{verticalAlign: 'super', fontSize: '0.8em', backgroundColor: 'inherit'} : null} >
 					{value? name[0] : name[1]}
-				</span>
+				</div>
 		</div>
 	)
 
