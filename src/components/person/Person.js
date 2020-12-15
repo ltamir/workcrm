f679ce2c7636b4ca3eea5876b90b370c585169b9
Lior Tamir <lior.tamir@gmail.com>
2020-12-15 11:16:30 +0200

    note continues the row in peron list

--- src/components/person/Person.js
+++ src/components/person/Person.js
@@ -3,25 +3,31 @@ import PersonType from './PersonType';
 import Field from '../gui/Field';
 import NotesField from '../gui/NotesField';
 import TitleField from '../gui/TitleField';
+import * as gui from '../gui/utils'
 import './Person.css'
 
 const Person = ({person, personType, onClick, onDblCLick}) =>(
 	
 	<div className="Person" onDoubleClick={onDblCLick? () => onDblCLick(person.id) : null} onClick={onClick? () => onClick(person): null}>
-		<div className="PersonRow">
-			<TitleField value={person.fullname} />
-			<span className="PersonRow" style={{marginLeft: '10px'}}>
-				{person.email && <Field icon="email" value={person.email} />}
-				<Field icon="phone" value={person.phone} />
-			</span>
-			<div className="PersonType">
-				Type: <PersonType personType={personType}/>
+			<div className="PersonRow">
+				<TitleField value={person.fullname} />
+				<span className="PersonRow" style={{marginLeft: '10px'}}>
+					{person.email && <Field icon="email" value={person.email} />}
+					<Field icon="phone" value={person.phone} />
+				</span>
+				<div className="PersonType">
+					Type: <PersonType personType={personType}/>
+				</div>
+				{!onClick && (
+					<span style={{marginLeft: '5px', flexGrow: 4}}>
+						<NotesField icon="note" value={person.notes}/>
+					</span>
+				)}
+
 			</div>
-		</div>
-		<div className="PersonRow">
-			<NotesField icon="note" value={person.notes}/>
-		</div>
-		
+			{onClick && (<div className="PersonRow" style={gui.getDirectionStyle(person.notes)}>
+				<NotesField icon="note" value={person.notes}/>
+			</div>)}
 	</div>
 )
 
